3770c9fa6382f756ea02675b4dfda26d01d1654b
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2024-02-01 00:35:39 +0100

    feat: added more features

--- protocol/contracts/Relayer.sol
+++ protocol/contracts/Relayer.sol
@@ -8,14 +8,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Relayer is Ownable {
 
+    error ZeroValueError();
+    error CallerNotExecutor();
+    error GasPriceExceededMaxPayPerView(uint _price, uint _maxPayPerView);
+
+    struct Campaign {
+        address sponsor;
+        uint maxPayPerView;
+        uint balance;
+    }
+
     event AddExecutor(address indexed _executor);
     event RemoveExecutor(address indexed _executor);
-    event CreateAdCampaign(address indexed _sponsor, uint indexed _campaignId, uint indexed funds);
+    event CreateAdCampaign(address indexed _sponsor, uint indexed _campaignId, uint _balance, uint _maxPayPerView);
+    event ExecuteOrder(uint indexed _campaignId, uint _fee, uint _balance);
 
-    mapping(address => bool) executor;
+    uint public campaignId = 1;
+    mapping(address => bool) public executor;
+    mapping(uint => Campaign) public campaigns;
+
+
+    uint16 public constant PERCENT = 10000;
+    uint16 public feePercent = 1; // 1 / 10000
     
     constructor() Ownable(msg.sender) {}
 
+    function createCampaign(address _sponsor, uint _maxPayPerView) external payable {
+        uint _campaignId = campaignId++;
+        uint value = msg.value;
+        if (value == 0) revert ZeroValueError();
+        campaigns[_campaignId] = Campaign({
+            sponsor: _sponsor,
+            maxPayPerView: _maxPayPerView,
+            balance: value
+        });
+        emit CreateAdCampaign(_sponsor, _campaignId, value, _maxPayPerView);
+    }
+
+    function executeOrder(uint _campaignId, address _destination, bytes calldata data) external onlyExecutor {
+        
+        Campaign storage _campaign = campaigns[_campaignId];
+
+        uint gasPrice = tx.gasprice;
+
+        uint payment = gasPrice * feePercent / PERCENT;
+
+        _campaign.balance -= payment;
+        
+        if (gasPrice > _campaign.maxPayPerView) revert GasPriceExceededMaxPayPerView(gasPrice, _campaign.maxPayPerView);
+        
+        (bool success, ) =  _destination.call(data);
+
+        payable(msg.sender).call{value: payment};
+
+        emit ExecuteOrder(_campaignId, payment, _campaign.balance);
+
+    }
 
 
     function addExecutor(address _executor) external onlyOwner {
@@ -26,6 +74,11 @@ contract Relayer is Ownable {
     function removeExecutor(address _executor) external onlyOwner {
         delete executor[_executor];
         emit RemoveExecutor(_executor);
+    }
+
+    modifier onlyExecutor() {
+        if (!executor[msg.sender]) revert CallerNotExecutor();
+        _;
     }
 
 
