559218d93d7f311c863cf8c58c0ecc4a734a4d50
techyNonso <williamikeji@gmail.com>
2024-02-26 19:56:56 +0100

    forking with bsc testnet done

--- client/src/pages/swap.tsx
+++ client/src/pages/swap.tsx
@@ -3,15 +3,15 @@ import { SwapTransaction, columns } from '@/components/Swap/Columns'
 import SwapForm from '@/components/Swap/SwapForm'
 import { SwapTransactionRecord } from '@/components/Swap/SwapTransactionRecord'
 import Nav from '@/components/ui/nav'
-import mydata from "@/lib/swapData.json"
+import myData from "@/lib/swapData.json"
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { TbArrowBigDownLinesFilled } from "react-icons/tb";
 
 export default function Swap() {
+    //! fix this before deployment .
     //@ts-ignore
-    const [data, setData] = useState<SwapTransaction[]>(mydata);
-
+    const [data, setData] = useState<SwapTransaction[]>(myData);
 
     return (
         <main className='h-screen '>
--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -4,8 +4,14 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
-
-  }
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://go.getblock.io/fa9c3b8a855d4dd5b786ece5594b8190",
+      },
+      chainId: 97,
+    },
+  },
 };
 
 export default config;
